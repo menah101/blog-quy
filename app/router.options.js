5ef3180c3aa2883d72b0cda83983fd65da96f124
quy_le_van <quy_le_van@bita.jp>
2023-11-02 16:41:58 +0700

    chore(scroll): remove scroll to

--- app/router.options.js
+++ app/router.options.js
@@ -1,17 +1,17 @@
 // https://router.vuejs.org/api/interfaces/routeroptions.html
 export default {
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
+  // scrollBehavior(to, from, savedPosition) {
+  //   if (savedPosition) {
+  //     return savedPosition;
+  //   }
 
-    if (to.hash) {
-      return {
-        el: to.hash,
-        behavior: "smooth",
-      };
-    }
+  //   if (to.hash) {
+  //     return {
+  //       el: to.hash,
+  //       behavior: "smooth",
+  //     };
+  //   }
 
-    return { left: 0, top: 30, behavior: 'smooth' };
-  },
+  //   return { left: 0, top: 30, behavior: 'smooth' };
+  // },
 };
